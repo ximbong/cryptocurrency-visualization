75e9a947acb921c4d50d7d5c32f0e9c41db920c5
Hai Truong <haitruong181096@gmail.com>
2018-06-07 09:28:27 +0300

    Add some filter changes

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
       } = element;
       stringHTML += `<div class="coin">
         <div class="coin-name" id="${name}">
-          <img src="https://s2.coinmarketcap.com/static/img/coins/16x16/${id}.png" alt=""> ${name}
+          <img src="https://s2.coinmarketcap.com/static/img/coins/32x32/${id}.png" alt=""> ${name}
         </div>
         <div class="rank">
           Rank: <span>${rank}</span>
@@ -37,115 +37,118 @@ document.addEventListener("DOMContentLoaded", function(event) {
       percentage_array.forEach(function(element, index) {
         if (!element) {
           stringHTML += `
-              <div class="${Object.keys({element})[0]}">
+              <div class="${Object.keys({
+            element})[0]}">
                 Change last ${time_array[index]}: <span>unknown</i></span>
               </div>
           `;
-        } else if (element > 0) {
-          stringHTML += `
-              <div class="${Object.keys({element})[0]}">
+          } else if (element > 0) {
+            stringHTML += `
+              <div class="${Object.keys({
+              element})[0]}">
                 Change last ${time_array[index]}: <span class="increase">${element}% <i class="fas fa-caret-up"></i></span>
               </div>
           `;
-          } else {
-            stringHTML += `
-              <div class="${Object.keys({element})[0]}">
+            } else {
+              stringHTML += `
+              <div class="${Object.keys({
+                element})[0]}">
                 Change last ${time_array[index]}: <span class="decrease">${element}% <i class="fas fa-caret-down"></i></span>
               </div>
           `;
-            }});
-          stringHTML += `</div></div>`
+              }});
+            stringHTML += `</div></div>`
+          };
+
+          document.querySelector(".container").innerHTML += stringHTML;
         };
 
-        document.querySelector(".container").innerHTML += stringHTML;
-      }
+        //empty the div, set index to 100 (basic)
+        const reset = () => {
+          document.querySelector(".container").innerHTML = "";
+          index = 100;
+        }
 
-      //empty the div, set index to 100 (basic)
-      const reset = () => {
-        document.querySelector(".container").innerHTML = "";
-        index = 100;
-      }
+        //fetch data from an url, and push into dataArray
+        const fetchFunction = (url) => {
+          fetch(url).then(function(response) {
+            return response.json()
+          }).then(function(response) {
+            dataArray.push(...response.data)
+          })
+        }
 
-      //fetch data from an url, and push into dataArray
-      const fetchFunction = (url) => {
-        fetch(url).then(function(response) {
+        //sort with different criteria
+        const handleSort = (criteria) => {
+          if (criteria === 'price') {
+            dataArray.sort(function(a, b) {
+              return b.quotes.USD.price - a.quotes.USD.price;
+            });
+          }
+          if (criteria === 'name') {
+            dataArray.sort(function(a, b) {
+              var nameA = a.name.toUpperCase();
+              var nameB = b.name.toUpperCase();
+              if (nameA < nameB) {
+                return -1;
+              }
+              if (nameA > nameB) {
+                return 1;
+              }
+            });
+          }
+          if (criteria === 'rank') {
+            dataArray.sort(function(a, b) {
+              return a.rank - b.rank;
+            });
+          }
+          reset();
+          render(dataArray.slice(0, 100))
+          document.querySelector('input').value = "";
+        }
+
+        //fetch first 100 coins when page loads
+        fetch(baseUrl).then(function(response) {
           return response.json()
         }).then(function(response) {
           dataArray.push(...response.data)
+          render(dataArray)
+        }).then(function(response) { //fetch the rest when page finish rendering
+          for (let i = 1; i < 16; i++) {
+            const url = baseUrl + `&start=${i * 100 + 1}`;
+            setTimeout(function() {
+              fetchFunction(url)
+            }, i * 100);
+          }
+        });
+
+        document.addEventListener('click', function(event) {
+
+          //if click on the load-more button
+          if (event.target.classList.contains('load-more')) {
+            render(dataArray.slice(index, index + 100))
+            index += 100;
+          }
+
+          //the target id has the same name with criteria name in handleSort declaration
+          if (event.target.classList.contains('sort')) {
+            handleSort(event.target.id);
+          }
+        });
+
+        document.querySelector('input').addEventListener('keyup', function(event) {
+          let value = this.value;
+          if (value !== "") { //if value is not empty
+            let filterArray = dataArray.filter(function(element) {
+              return element.name.toLowerCase().includes(value.toLowerCase())
+            })
+            reset();
+            render(filterArray.slice(0, 200));
+            document.querySelector('.load-more').style.display = 'none';
+          } else { //if value is empty
+            reset();
+            render(dataArray.slice(0, 100))
+            document.querySelector('.load-more').style.display = 'flex';
+          }
         })
-      }
-
-      //sort with different criteria
-      const handleSort = (criteria) => {
-        if (criteria === 'price') {
-          dataArray.sort(function(a, b) {
-            return b.quotes.USD.price - a.quotes.USD.price;
-          });
-        }
-        if (criteria === 'name') {
-          dataArray.sort(function(a, b) {
-            var nameA = a.name.toUpperCase();
-            var nameB = b.name.toUpperCase();
-            if (nameA < nameB) {
-              return -1;
-            }
-            if (nameA > nameB) {
-              return 1;
-            }
-          });
-        }
-        if (criteria === 'rank') {
-          dataArray.sort(function(a, b) {
-            return a.rank - b.rank;
-          });
-        }
-        reset();
-        render(dataArray.slice(0, 100))
-        document.querySelector('input').value = "";
-      }
-
-      //fetch first 100 coins when page loads
-      fetch(baseUrl).then(function(response) {
-        return response.json()
-      }).then(function(response) {
-        dataArray.push(...response.data)
-        render(dataArray)
-      }).then(function(response) { //fetch the rest when page finish rendering
-        for (let i = 1; i < 16; i++) {
-          const url = baseUrl + `&start=${i * 100 + 1}`;
-          setTimeout(function() {
-            fetchFunction(url)
-          }, i * 100);
-        }
-      })
-
-      document.addEventListener('click', function(event) {
-
-        //if click on the load-more button
-        if (event.target.classList.contains('load-more')) {
-          render(dataArray.slice(index, index + 100))
-          index += 100;
-        }
-
-        //the target id has the same name with criteria name in handleSort declaration
-        if (event.target.classList.contains('sort')) {
-          handleSort(event.target.id);
-        }
       });
-
-      document.querySelector('input').addEventListener('keyup', function(event) {
-        let value = this.value;
-        if (value !== "") { //if value is not empty
-          let filterArray = dataArray.filter(function(element) {
-            return element.name.toLowerCase().includes(value.toLowerCase())
-          })
-          reset();
-          render(filterArray);
-          document.querySelector('.load-more').style.display = 'none';
-        } else { //if value is empty
-          reset();
-          render(dataArray.slice(0, 100))
-          document.querySelector('.load-more').style.display = 'flex';
-        }
-      })
-    });
